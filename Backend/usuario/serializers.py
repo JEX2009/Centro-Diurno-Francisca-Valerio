b1c8ec1b68b95b5730bbdb04e90e1af2da3a7d82
Jason <152582224+JEX2009@users.noreply.github.com>
2025-10-13 17:00:34 -0600

    feat usuario: agregar los usuarios

---
+++ Backend/usuario/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from . import models as m
+from django.db import transaction
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = m.Usuario
+        fields = ['id', 'username', 'first_name', 'last_name', 'codigo' , 'password']
+        
+    def create(self, validated_data):
+        worker = m.Usuario.objects.create_user(**validated_data)
+        return worker
---
+++ Backend/usuario/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from . import models as m
+from . import serializers as s
+# Create your views here.
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = m.Usuario.objects.all()
+    serializer_class = s.UsuarioSerializer
+    permission_classes = [permissions.AllowAny]
